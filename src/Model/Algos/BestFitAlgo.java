3b202ef98cd2e5fe78e42de96dc340d47e76b50f
Tyler W <jtwalder@gmail.com>
2018-11-19 13:20:10 -0500

    Added BestFitAlog

---
+++ src/Model/Algos/BestFitAlgo.java
@@ -0,0 +1,50 @@
+package Model.Algos;
+
+import Model.Process;
+
+import java.util.Optional;
+
+public class BestFitAlgo extends FitAlgo {
+    public BestFitAlgo(int totalmem) {
+        super(totalmem);
+    }
+
+    @Override
+    public Integer allocPs(Process unallocated) {
+        long procSize=super.getProcessSize(unallocated);
+        if(procSize>memory.length){
+            return null;
+        }
+        int bestStart=-1;
+        int bestLength=-1;
+        int start=0;
+        int length=0;
+        for(int i=0;i<memory.length;i++){
+            if(memory[i]){
+                if(length==procSize){
+                    unallocated.setBaseAddress(Optional.of(new Integer(start)));
+                    filler(start,length,true);
+                    return length;
+                }
+                if(length>procSize&&length<bestLength){
+                    bestStart=start;
+                    bestLength=length;
+                }
+                start=i+1;
+                length=0;
+            }
+            else{
+                length++;
+
+            }
+        }
+        if(bestLength!=-1&&bestStart!=-1){
+            unallocated.setBaseAddress(Optional.of(new Integer(bestStart)));
+            filler(bestStart,(int)procSize,true);
+            return bestStart;
+        }
+        return null;
+
+
+    }
+}
