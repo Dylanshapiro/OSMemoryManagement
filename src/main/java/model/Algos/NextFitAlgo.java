f498ae58a63856b0e29b59481b117c6ad2441bfc
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-03 02:13:21 -0500

    huh

--- src/main/java/model/Algos/NextFitAlgo.java
+++ src/main/java/model/Algos/NextFitAlgo.java
@@ -1,41 +1,50 @@
 package model.Algos;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import model.MemoryManager;
 
-public class NextFitAlgo implements Algo{
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Map.Entry;
 
+public class NextFitAlgo implements Algo {
 
-    private static Long startBase;
-    private static int startIndex = 0;
+    private static Long lastAllocatedAt = new Long(0);
 
     @Override
     public Long allocateP(Long procSize, HashMap<Long, Long> freeMem) {
+        System.out.println("totalMem: " + MemoryManager.getInstance().getMemSize());
+        System.out.println("lastAllocated at: " + lastAllocatedAt);
 
-        Object[] keys = freeMem.keySet().toArray();
-        Arrays.sort(keys);
+        Optional<Entry<Long, Long>> maybeBigEnough = trySearch(procSize, freeMem, lastAllocatedAt);
 
-        if (startBase == null) {
-            startBase = (long) 0;
+        // if we couldnt find a hole check vals that werent checked yet
+        if (!maybeBigEnough.isPresent()) {
+            maybeBigEnough = trySearch(procSize, freeMem, new Long(0));
         }
-        //calculate start index
-        //TODO figure out way to know what index in key array to start from
 
-        //how many iterations
-        int counter = 0;
-        //first fit but start at startAddress
-        for (int i = startIndex; counter < keys.length; i = (i + 1) % keys.length) {
-
-            Long baseAddress = (Long) keys[i];
-
-            if ((freeMem.get(baseAddress) - baseAddress) > procSize) {
-                startBase = baseAddress + 1;
-                return baseAddress;
-            }
-            counter ++;
+        // All vals have now been checked (including those before starting point )
+        // either a spot was found and it can be returned or there isnt one available
+        if(!maybeBigEnough.isPresent()){
+            System.out.println("not enough space to allocate");
+            return null;
+        } else {
+            Entry<Long, Long> bigEnough = maybeBigEnough.get();
+            lastAllocatedAt = bigEnough.getKey();
+            System.out.println("allocating at : " +  bigEnough.getKey());
+            return bigEnough.getKey();
         }
-        return null;
     }
+
+    private Optional<Entry<Long, Long>> trySearch(Long procSize, HashMap<Long, Long> freeMem, Long startPoint) {
+        return freeMem.entrySet()
+                .stream()
+                .sorted((a, b) -> Long.valueOf(a.getKey()).compareTo(Long.valueOf(b.getKey()))) // sort entries by base
+                .filter(entry -> entry.getKey() >= startPoint) // throw away entries with base < last allocated
+                .filter(entry -> entry.getValue() > procSize) //throw away entries cant fit the proc
+                .findFirst(); // will get first proc that is left if there is one
+    }
+
 
     @Override
     public String getName() {
